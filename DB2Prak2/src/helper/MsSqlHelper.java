a68f2d15d5e173891a2e581a873d4634d0ef5d2c
incredibleXE <fabian.ranacher@gmail.com>
2015-03-24 11:26:45 +0100

    added some functions for connect, close, sql exception parser, ignoring sql exceptions

--- DB2Prak2/src/helper/MsSqlHelper.java
+++ DB2Prak2/src/helper/MsSqlHelper.java
@@ -1,8 +1,10 @@
 package helper;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class MsSqlHelper {
@@ -19,11 +21,88 @@ public class MsSqlHelper {
 		this.user = user;
 		this.passwd = passwd;
 	}
-	
-	public MsSqlHelper(String host, String database, String user, String passwd,boolean winlogon) {
+
+	public MsSqlHelper(String host, String database, String user,
+			String passwd, boolean winlogon) {
 		this.database = database;
 		this.host = host;
 		this.user = user;
 		this.passwd = passwd;
+	}
+
+	private Connection connect() {
+		try {
+			String url = "jdbc:sqlserver://" + host + ";databaseName="
+					+ database;
+			if (winlogon == false) {
+				Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+				connect = DriverManager.getConnection(url, user, passwd);
+			} else {
+				url = url + ";integratedSecurity=true";
+				Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+				connect = DriverManager.getConnection(url);
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			printSQLException(e);
+		}
+		return connect;
+	}
+
+	public static void printSQLException(SQLException ex) {
+
+		for (Throwable e : ex) {
+			if (e instanceof SQLException) {
+				if (ignoreSQLException(((SQLException) e).getSQLState()) == false) {
+
+					e.printStackTrace(System.err);
+					System.err.println("SQLState: "
+							+ ((SQLException) e).getSQLState());
+
+					System.err.println("Error Code: "
+							+ ((SQLException) e).getErrorCode());
+
+					System.err.println("Message: " + e.getMessage());
+
+					Throwable t = ex.getCause();
+					while (t != null) {
+						System.out.println("Cause: " + t);
+						t = t.getCause();
+					}
+				}
+			}
+		}
+	}
+
+	public static boolean ignoreSQLException(String sqlState) {
+		if (sqlState == null) {
+			System.out.println("The SQL state is not defined!");
+			return false;
+		}
+
+		// X0Y32: Jar file already exists in schema
+		if (sqlState.equalsIgnoreCase("X0Y32"))
+			return true;
+
+		// 42Y55: Table already exists in schema
+		if (sqlState.equalsIgnoreCase("42Y55"))
+			return true;
+
+		return false;
+	}
+	
+	public ResultSet executeSQLQuery(String query) {
+		try {
+			connect = connect();
+			// Statements allow to issue SQL queries to the database
+			statement = connect.createStatement();
+			// Result set get the result of the SQL query
+			resultSet = statement
+					.executeQuery("select * from feedback.comments");
+		} catch (SQLException e) {
+			printSQLException(e);
+		}
+		return resultSet;
 	}
 }
