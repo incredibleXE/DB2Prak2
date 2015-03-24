be3608e23fcf4c76c8bfc7fbde031b1d85f05f49
incredibleXE <fabian.ranacher@gmail.com>
2015-03-24 11:41:38 +0100

    added preparedStatement function

--- DB2Prak2/src/helper/MsSqlHelper.java
+++ DB2Prak2/src/helper/MsSqlHelper.java
@@ -10,9 +10,9 @@ import java.sql.Statement;
 public class MsSqlHelper {
 	private Connection connect = null;
 	private Statement statement = null;
-	private PreparedStatement preparedStatement = null;
 	private ResultSet resultSet = null;
 	private String database = null, user = null, passwd = null, host = null;
+	private PreparedStatement preparedStatement = null;
 	private boolean winlogon = false;
 
 	public MsSqlHelper(String host, String database, String user, String passwd) {
@@ -49,7 +49,24 @@ public class MsSqlHelper {
 		}
 		return connect;
 	}
-
+	
+	/**
+	 * testing the connection
+	 * 
+	 * @return boolean - true -> connection is working | false -> not
+	 */
+	public boolean testConnection() {
+		if(connect!=null) {
+			close();
+		}
+		connect = connect();
+		if(connect!=null) {
+			close();
+			return true;
+		}
+		return false;
+	}
+	
 	public static void printSQLException(SQLException ex) {
 
 		for (Throwable e : ex) {
@@ -91,7 +108,7 @@ public class MsSqlHelper {
 
 		return false;
 	}
-	
+
 	public ResultSet executeSQLQuery(String query) {
 		try {
 			connect = connect();
@@ -103,6 +120,52 @@ public class MsSqlHelper {
 		} catch (SQLException e) {
 			printSQLException(e);
 		}
+		close();
 		return resultSet;
+	}
+	
+	/**
+	 * preparedStatement way of talking to the database
+	 *  
+	 * @param statement SQL Query with ?
+	 * @param arguments String array with arguments in order to their places in the statement
+	 * @return resultSet the answer from database
+	 */
+	public ResultSet executePreparedStatement(String statement, String[] arguments) {
+		connect = connect();
+		try {
+			preparedStatement = connect.prepareStatement(statement);
+			for(int i=1;arguments.length>=i;i++) {
+				preparedStatement.setString(i, arguments[i-1]);
+			}
+			resultSet = preparedStatement.executeQuery();
+			return resultSet;
+		} catch (SQLException e) {
+			printSQLException(e);
+		} finally {
+			close();
+		}
+		return null;
+	}
+
+	/**
+	 * closes everything in the right order
+	 */
+	private void close() {
+		try {
+			if (resultSet != null) {
+				resultSet.close();
+			}
+
+			if (statement != null) {
+				statement.close();
+			}
+
+			if (connect != null) {
+				connect.close();
+			}
+		} catch (Exception e) {
+
+		}
 	}
 }
